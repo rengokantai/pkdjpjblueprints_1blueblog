f0706b875cd185d9be6505a58c15389ce576dada
Hernan Y.Ke <yuriqiao@gmail.com>
2016-06-15 16:57:24 -0400

    Add templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.core.urlresolvers import reverse
+from django.views.generic import CreateView
 
 # Create your views here.
+
+
+class UserRegistrationView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'user_registration.html'
+    def get_success_url(self):
+        return reverse('home')
--- pkdjpjblueprints_1blueblog/settings.py
+++ pkdjpjblueprints_1blueblog/settings.py
@@ -105,3 +105,4 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
+LOGIN_REDIRECT_URL = '/'
